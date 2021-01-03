046b880fb7a2874121fe02ba9a2622533f14f19a
AndrewKraft <43654559+AndrewKraft@users.noreply.github.com>
2021-01-02 17:45:54 -0800

    work done on file loading function

--- src/loader.py
+++ src/loader.py
@@ -1,8 +1,40 @@
-import gzip
+import gzip, numpy as np
 
 def load_idx(fname):
+
+	# open and unzip file with gzip
 	with gzip.open(fname, 'rb') as f:
-		magic_num = f.read(4)
-		data_type = magic_num[2]
-		dimensions = magic_num[3]
-	return magic_num, data_type, dimensions
+
+		# idx file format starts with magic num, detailing the type of data contained and the shape of the container
+		magic_num = int.from_bytes(f.read(4), "big")
+		data_type = (magic_num >> 8) & 0xff
+		dimensions = magic_num & 0xff
+
+		# empty tuple for the shape of the data for use with np.array
+		shape = ()
+		for i in range(dimensions):
+
+			# create 1 dimensional tuple for size for easy concatenation
+			size_i = (int.from_bytes(f.read(4), "big"),)
+			shape += size_i
+
+		data = np.empty(shape)
+		
+		# initialize index to iterate through dataset
+		index = (0,) * dimensions
+		while True:
+			data[index] = int.from_bytes(f.read(1), "big")
+			
+			# iterate counter, if count in dimension equals size of dimension, reset and increment next dimension
+			ndim = dimensions - 1
+			while ndim > -1:
+				index[ndim] += 1
+				if index[ndim] == shape[ndim]:
+					index[ndim] = 0
+					ndim -= 1
+				else:	break
+			else:	break
+
+	return data, shape
+		
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 # python libraries
 import matplotlib.pyplot as plt
 from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
 
 # local libraries
 import loader
 
-n, t, d = loader.load_idx("./data/train-images-idx3-ubyte.gz")
-print("n:\t%s\nt:\t%s\nd:\t%s\n" % (n, t, d))
+trainX, shape = loader.load_idx("./data/train-images-idx3-ubyte.gz")
+print("shape:\t{shape}")
